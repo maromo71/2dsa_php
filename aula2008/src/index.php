afe81e8fe299c039530ab44b1d7257674cbfd191
professormoraes@gmail.com <professormoraes@gmail.com>
2024-08-20 11:25:17 -0300

    Aula do dia 20 de agosto

--- aula2008/src/index.php
+++ aula2008/src/index.php
@@ -11,6 +11,35 @@
     //pegando os dados da consulta
     $alunos = $stmt->fetchAll();
     //mostrando os dados format json
-    echo json_encode($alunos);
+    //echo json_encode($alunos);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados dos alunos</title>
+</head>
+<body>
+    <h1>Dados dos alunos</h1>
+    <h2>Matriculados</h2>
+    <table>
+        <tr>
+            <th>RM</th>
+            <th>Nome</th>
+            <th>Email</th>
+        </tr>
+        <?php
+            foreach ($alunos as $aluno) {
+                echo "<tr>";
+                echo "<td>" . $aluno['rm'] . "</td>";
+                echo "<td>" . $aluno['nome'] . "</td>";
+                echo "<td>" . $aluno['email'] . "</td>";
+                echo "</tr>";
+            }
+        ?>
+        </table>
+</body>
+</html>
 
 
